eab20863dc2866004d06659adce16d2fdf8633a1
seung9066 <seoung9066@naver.com>
2024-01-28 17:16:59 +0900

    공통코드 컨트롤러

--- src/main/java/com/sgg/MainController.java
+++ src/main/java/com/sgg/MainController.java
@@ -1,32 +1,22 @@
 package com.sgg;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.thymeleaf.util.MapUtils;
-import org.thymeleaf.util.StringUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
-
 
 @Controller
 public class MainController {
+
+    /**
+     * 메인페이지
+     * @return
+     */
     @RequestMapping("/")
     public String index() {
+
         return "index";
     }
 
@@ -63,52 +53,6 @@ public class MainController {
         return formData.get("pagePath");
     }
 
-    @PostMapping("/getSession")
-    @ResponseBody
-    public Map<String, Object> getSession(@RequestBody Map<String, String> map, HttpSession session) {
-        Map<String, Object> returnMap = new HashMap<>();
-        if (!StringUtils.isEmpty(map.get("sessionId"))) {
-            if (map.get("sessionId").equals("userInfo")) {
-                returnMap.put("userId", session.getAttribute("userId"));
-                returnMap.put("userNm", session.getAttribute("userNm"));
-                returnMap.put("userAuth", session.getAttribute("userAuth"));
-            } else {
-                returnMap.put(map.get("sessionId"), session.getAttribute(map.get("sessionId")));
-            }
-        }
 
-        return returnMap;
-    }
-
-    @PostMapping("/upLoadFile")
-    @ResponseBody
-    public int upLoadFile(MultipartHttpServletRequest files) throws IOException {
-        int cnt = 0;
-        List<MultipartFile> fileList = files.getFiles("files");
-
-        Path currentPath = Paths.get("").toAbsolutePath();
-        String uploadFolder = currentPath + "/src/main/resources/file";
-
-        for (int i = 0; i < fileList.size(); i++) {
-            long size = fileList.get(i).getSize();
-            if (size > 0) {
-                UUID uuid = UUID.randomUUID();
-                String fileUuid = uuid.toString().replaceAll("-", "");
-                String fileRealName = fileList.get(i).getOriginalFilename();
-                String fileType = fileList.get(i).getContentType();
-
-                File saveFile = new File(uploadFolder + "\\" + fileUuid + fileRealName);
-                try {
-                    fileList.get(i).transferTo(saveFile);
-                    cnt++;
-                } catch (IllegalStateException | IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return cnt;
-    }
 
 }
--- src/main/java/com/sgg/cmn/controller/CmnController.java
+++ src/main/java/com/sgg/cmn/controller/CmnController.java
@@ -1,18 +1,25 @@
 package com.sgg.cmn.controller;
 
 import com.sgg.cmn.service.CmnService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.apache.ibatis.session.SqlSession;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.thymeleaf.util.MapUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.thymeleaf.util.StringUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 
 @Controller
@@ -60,11 +67,77 @@ public class CmnController {
         return cmnService.getData(map, param);
     }
 
+    /**
+     * 단건, 다건 업데이트
+     * @param map
+     * @return
+     * @throws Exception
+     */
     @PostMapping("/cmn/upData")
     @ResponseBody
     public int upData(@RequestBody Map<String, Object> map) throws Exception{
 
         return cmnService.upData(map);
+    }
+
+    /**
+     * 세션 정보 가져오기
+     * @param map
+     * @param session
+     * @return
+     */
+    @PostMapping("/getSession")
+    @ResponseBody
+    public Map<String, Object> getSession(@RequestBody Map<String, String> map, HttpSession session) {
+        Map<String, Object> returnMap = new HashMap<>();
+        if (!StringUtils.isEmpty(map.get("sessionId"))) {
+            if (map.get("sessionId").equals("userInfo")) {
+                returnMap.put("userId", session.getAttribute("userId"));
+                returnMap.put("userNm", session.getAttribute("userNm"));
+                returnMap.put("userAuth", session.getAttribute("userAuth"));
+            } else {
+                returnMap.put(map.get("sessionId"), session.getAttribute(map.get("sessionId")));
+            }
+        }
+
+        return returnMap;
+    }
+
+    /**
+     * 파일 업로드
+     * @param files
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/upLoadFile")
+    @ResponseBody
+    public int upLoadFile(MultipartHttpServletRequest files) throws IOException {
+        int cnt = 0;
+        List<MultipartFile> fileList = files.getFiles("files");
+
+        Path currentPath = Paths.get("").toAbsolutePath();
+        String uploadFolder = currentPath + "/src/main/resources/file";
+
+        for (int i = 0; i < fileList.size(); i++) {
+            long size = fileList.get(i).getSize();
+            if (size > 0) {
+                UUID uuid = UUID.randomUUID();
+                String fileUuid = uuid.toString().replaceAll("-", "");
+                String fileRealName = fileList.get(i).getOriginalFilename();
+                String fileType = fileList.get(i).getContentType();
+
+                File saveFile = new File(uploadFolder + "\\" + fileUuid + fileRealName);
+                try {
+                    fileList.get(i).transferTo(saveFile);
+                    cnt++;
+                } catch (IllegalStateException | IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return cnt;
     }
 
 }
